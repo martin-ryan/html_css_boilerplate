eb4923ed9329845d7346aaaf05253b86b5a330d8
Martin Ryan <martinrryan@gmail.com>
2015-10-29 16:23:08 -0500

    add js - mission prompts

--- scripts/apps.js
+++ scripts/apps.js
@@ -1,6 +1,13 @@
 
 window.onload = function () {
-  statusReport('hey hey hey')
+
+  var username = prompt('what is your name?');
+  var mission = prompt('what is your mission?');
+
+  var contentContainer = document.getElementById('content');
+
+  contentContainer.innerHTML = username + 'is on a mission to ' + mission;
+
 }
 
 function statusReport(message) {
